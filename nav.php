1a3224021829ab8108bc97c91b527072d3608d21
bwilli16 <33357883+bwilli16@users.noreply.github.com>
2017-11-27 14:35:52 -0500

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,54 @@
+<!-- ######################     Start of Nav   ########################## -->
+<nav>
+    <ol>
+        <?php
+        print '<li class="';
+        if ($path_parts['filename'] == "home") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="home.php">Home</a>';
+        print '</li>';
+        //****
+        print '<li class="';
+        if ($path_parts['filename'] == "about") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="about.php">About</a>';
+        print '</li>';
+        //****
+        print '<li class="';
+        if ($path_parts['filename'] == "baking") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="baking.php">Baking</a>';
+        print '</li>';
+        //****
+        print '<li class="';
+        if ($path_parts['filename'] == 'cooking') {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="cooking.php">Cooking</a>';
+        print '</li>';
+        //****
+        print '<li class="';
+        if ($path_parts['filename'] == 'essentials') {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="essentials.php">Essentials</a>';
+        print '</li>';
+        //****
+        print '<li class="';
+        if ($path_parts['filename'] == "subscribe") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="subscribe.php">Subscribe</a>';
+        print '</li>';
+        ?>
+    </ol>
+</nav>
