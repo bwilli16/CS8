33e6176c5e9096b0f6dc61f3b736cb9bef576ee8
sgaug <33431654+sgaug@users.noreply.github.com>
2017-11-27 14:33:12 -0500

    Create top.php

---
+++ top.php
@@ -0,0 +1,12 @@
+// 11/27: this is just so you can see the metadata I have. Feel free to edit.
+
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <title>We Need A Name</title>
+        <meta name="author" content="Blake, Erin, & Sarah">
+        <meta charset="utf-8">
+        <meta name="description" content="Baking, cooking and nutrition hacks for college students/young adults on a budget">
+        <link rel="stylesheet" href="../css/project.css" type="text/css" media="screen">
+    </head>
+</html>
