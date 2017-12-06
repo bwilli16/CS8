922ab7a2e428d6723c9dc205d9d9e524b5d22907
bwilli16 <33357883+bwilli16@users.noreply.github.com>
2017-12-05 21:08:40 -0500

    Update essentials.php

--- essentials.php
+++ essentials.php
@@ -1,6 +1,114 @@
-2: essential ingredients (facts about frequently used/nutritious ingredients)
-
 <?php
 include ('top.php');
-
 ?>
+
+<body>
+ 
+<div class="row">
+    <figure class="ingredient one">
+        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
+        <figcaption> <b>Title of the FIRST Recipe </b><br>
+                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+    <figure class="ingredient two">
+        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
+        <figcaption> <b>Title of the SECOND Recipe</b> <br>
+                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+    <figure class="ingredient three">
+        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
+        <figcaption> <b>Title of the FIRST Recipe </b><br>
+                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+    <figure class="ingredient four">
+        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
+        <figcaption> <b>Title of the SECOND Recipe</b> <br>
+                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+</div>
+    <div class="row">
+    <figure class="ingredient one">
+        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
+        <figcaption> <b>Title of the FIRST Recipe </b><br>
+                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+    <figure class="ingredient two">
+        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
+        <figcaption> <b>Title of the SECOND Recipe</b> <br>
+                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+    <figure class="ingredient three">
+        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
+        <figcaption> <b>Title of the FIRST Recipe </b><br>
+                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+    <figure class="ingredient four">
+        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
+        <figcaption> <b>Title of the SECOND Recipe</b> <br>
+                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+</div>
+</div>
+    <div class="row">
+    <figure class="ingredient one">
+        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
+        <figcaption> <b>Title of the FIRST Recipe </b><br>
+                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+    <figure class="ingredient two">
+        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
+        <figcaption> <b>Title of the SECOND Recipe</b> <br>
+                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+    <figure class="ingredient three">
+        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
+        <figcaption> <b>Title of the FIRST Recipe </b><br>
+                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+    <figure class="ingredient four">
+        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
+        <figcaption> <b>Title of the SECOND Recipe</b> <br>
+                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+</div>
+</div>
+    <div class="row">
+    <figure class="ingredient one">
+        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
+        <figcaption> <b>Title of the FIRST Recipe </b><br>
+                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+    <figure class="ingredient two">
+        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
+        <figcaption> <b>Title of the SECOND Recipe</b> <br>
+                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+    <figure class="ingredient three">
+        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
+        <figcaption> <b>Title of the FIRST Recipe </b><br>
+                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+    <figure class="ingredient four">
+        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
+        <figcaption> <b>Title of the SECOND Recipe</b> <br>
+                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+</div>
+    
+</body>
