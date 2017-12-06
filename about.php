27d75004100a6266306387ca051a46733d2c2ba1
sgaug <33431654+sgaug@users.noreply.github.com>
2017-12-06 16:27:32 -0500

    Update about.php

--- about.php
+++ about.php
@@ -28,10 +28,9 @@ include ('top.php');
             neque mauris nec.</p>
     </div>
     <div id='col'>
-        <img id='portrait' alt="Portrait" src="../images/ice-in.jpg">
-        <p>Lorem ipsum dolor sit amet, arcu nunc, aenean dolor sem, in mauris donec, pellentesque amet risus sed sem viverra volutpat, 
-            suspendisse eu adipiscing gravida. Mauris commodo semper odio aliquam, venenatis curabitur interdum nulla. Adipiscing cum enim, 
-            risus cum nunc dolor enim mattis odio, pede curabitur facilisis, egestas vel lectus lorem pellentesque cras sit, nec sodales accumsan
-            neque mauris nec.</p>
+        <img id='portrait' alt="Portrait" src="../images/biosarah">
+        <p><b>Sarah</b> is a freshman at the University of Vermont, majoring in Political Science and minoring in Public Policy Analysis. She enjoys
+            reading (both classic and contemporary lit), traveling to new places, and snuggling with her Cocker Spaniel when she's back home in North Carolina.
+            When she's not writing papers or cramming for a test, you can find her sipping coffee, knitting, and watching reruns of crime dramas.</p>
     </div>
 </body>
