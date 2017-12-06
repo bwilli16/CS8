7e846a2b29dc064d8e4968ae9e1b2d56f4c4b3ef
bwilli16 <33357883+bwilli16@users.noreply.github.com>
2017-12-06 15:10:55 -0500

    Update about.php

--- about.php
+++ about.php
@@ -1,4 +1,37 @@
 <?php
 include ('top.php');
-
 ?>
+
+<body>
+    <div id='about'>
+        <p>Lorem ipsum dolor sit amet, arcu nunc, aenean dolor sem, in mauris donec, pellentesque amet risus sed sem viverra volutpat, 
+            suspendisse eu adipiscing gravida. Mauris commodo semper odio aliquam, venenatis curabitur interdum nulla. Adipiscing cum enim, 
+            risus cum nunc dolor enim mattis odio, pede curabitur facilisis, egestas vel lectus lorem pellentesque cras sit, nec sodales accumsan
+            neque mauris nec. Fusce nunc, vel at, nam dignissim, nisl non donec volutpat. Dictum morbi laoreet eligendi eleifend. Vitae eget. 
+            Sed ac velit dui ante integer egestas, diam mi ultricies elit vehicula etiam luctus, felis adipiscing, wisi ac mi, id interdum. 
+            Malesuada vulputate et, libero eros adipiscing eu vestibulum mus tempus, euismod mollis tempus nec vel a, amet nec dictum vestibulum. 
+            Ipsum in mollis ultricies, ante condimentum eget eros nec pede, ligula nisl id cras diam, velit ut feugiat, condimentum montes lectus vulputate nibh.
+        </p>
+    </div>
+    <div id='col'>
+        <img id='portrait' alt="Portrait" src="../images/ice-in.jpg">
+        <p>Lorem ipsum dolor sit amet, arcu nunc, aenean dolor sem, in mauris donec, pellentesque amet risus sed sem viverra volutpat, 
+            suspendisse eu adipiscing gravida. Mauris commodo semper odio aliquam, venenatis curabitur interdum nulla. Adipiscing cum enim, 
+            risus cum nunc dolor enim mattis odio, pede curabitur facilisis, egestas vel lectus lorem pellentesque cras sit, nec sodales accumsan
+            neque mauris nec.</p>
+    </div>
+    <div id='col'>
+        <img id='portrait' alt="Portrait" src="../images/ice-in.jpg">
+        <p>Lorem ipsum dolor sit amet, arcu nunc, aenean dolor sem, in mauris donec, pellentesque amet risus sed sem viverra volutpat, 
+            suspendisse eu adipiscing gravida. Mauris commodo semper odio aliquam, venenatis curabitur interdum nulla. Adipiscing cum enim, 
+            risus cum nunc dolor enim mattis odio, pede curabitur facilisis, egestas vel lectus lorem pellentesque cras sit, nec sodales accumsan
+            neque mauris nec.</p>
+    </div>
+    <div id='col'>
+        <img id='portrait' alt="Portrait" src="../images/ice-in.jpg">
+        <p>Lorem ipsum dolor sit amet, arcu nunc, aenean dolor sem, in mauris donec, pellentesque amet risus sed sem viverra volutpat, 
+            suspendisse eu adipiscing gravida. Mauris commodo semper odio aliquam, venenatis curabitur interdum nulla. Adipiscing cum enim, 
+            risus cum nunc dolor enim mattis odio, pede curabitur facilisis, egestas vel lectus lorem pellentesque cras sit, nec sodales accumsan
+            neque mauris nec.</p>
+    </div>
+</body>
