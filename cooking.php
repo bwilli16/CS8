2fae5dae8abf123d0c6a437d395af3e278a1a329
bwilli16 <33357883+bwilli16@users.noreply.github.com>
2017-12-05 20:00:05 -0500

    Update cooking.php

--- cooking.php
+++ cooking.php
@@ -1,5 +1,105 @@
-3: college cooking (easy/nutritious recipes)
 <?php
 include ('top.php');
-
 ?>
+
+<div id="head">
+    <h4>Cooking Recipes</h4>
+</div>
+<article class="baking cooking">
+    <div>
+        <div id="space">
+            <h5>.</h5>
+        </div>
+        <h2>Maple Glazed Salmon</h2>
+        <figure>
+            <img alt="Pic of salmon" src="../images/salmon.jpg">
+        </figure>
+        <p><b>Ingredients:</b></p>
+        <ol>
+            <li>6-12oz salmon filet </li>
+            <li>1/4 cup maple syrup </li>
+            <li>brown rice </li>
+            <li>kale </li>
+            <li>1/4 cup water </li>
+            <li>1tsp olive oil </li>
+            <li>salt and pepper to taste</li>
+        </ol>
+        <p>
+            Begin by preparing rice. While it's cooking, preheat oven to 400 degrees. If the salmon filet is large, cut in half or in thirds.
+            Line a baking pan with aluminum foil or parchment paper. Place the salmon on the pan
+            and coat with half of the maple syrup. Bake for 15 minutes, then briefly remove to 
+            re-coat with the rest of the maple syrup. Bake for an additional 5-10 minutes, depending
+            on the thickness of the salmon.
+        </p>
+        <p>
+            Chop desired amount of kale if not already done so. In a pan over medium heat, add water,
+            oil, and kale. Cover. Remove cover after a few minutes, sautee until kale is soft. Add salt and pepper to tase.
+        </p>
+         <p>
+            Serve salmon and kale over rice.
+        </p>
+        <h3>By Blake Williams. December 02, 2017</h3>
+    </div>
+    
+    <div>
+        <div id="space">
+            <h5>.</h5>
+        </div>
+        <h2>Chicken Fajita</h2>
+        <figure>
+            <img alt="Pic of fajita" src="../images/fajita.jpg">
+        </figure>
+        <p><b>Ingredients:</b></p>
+        <ol>
+            <li>2-3 chicken breasts </li>
+            <li>1 bell pepper </li>
+            <li>1tbsp olive oil </li>
+            <li>taco/fajita seasoning mix </li>
+            <li>tortillas </li>
+            <li>shredded cheese </li>
+            <li>lettuce </li>
+        </ol>
+        <p>
+            Begin by slicing the chicken into small pieces. Place half of the olive oil
+            in a pan and heat over medium-high heat. Place the chicken in the pan and sprinkled
+            the taco or fajita seasoning over it. Stir occasionally.
+        </p>
+        <p>
+            Cut up a bell pepper and add to the pan after about 5-10 minutes. Add the second half of the
+            olive oil. Keep stirring the pan periodically. Cook until chicken is thoroughly cooked, about 20 minutes.
+        </p>
+         <p>
+            Heat up the tortillas in the pan for about a minute per side or microwave for under a minute.
+            Add the chicken and peppers to the fajitas, and top with shredded lettuce, cheese, and any additionally
+            toppings you may prefer.
+        </p>
+        <h3>By Blake Williams. December 01, 2017</h3>
+    </div>
+    
+    <div>
+        <div id="space">
+            <h5>.</h5>
+        </div>
+        <h2>Third recipe title</h2>
+        <figure>
+            <img alt="Pic of food" src="../images/valley-view.jpg">
+        </figure>
+        <p><b>Ingredients:</b></p>
+        <ol>
+            <li>first </li>
+            <li>second </li>
+            <li>third </li>
+            <li>fourth ingredient </li>
+        </ol>
+        <p>
+            Lorem ipsum dolor sit amet, arcu nunc, aenean dolor sem, in mauris donec, pellentesque amet risus sed sem viverra volutpat, 
+            suspendisse eu adipiscing gravida. Mauris commodo semper odio aliquam, venenatis curabitur interdum nulla. Adipiscing cum enim, 
+            risus cum nunc dolor enim mattis odio, pede curabitur facilisis, egestas vel lectus lorem pellentesque cras sit, nec sodales accumsan
+            neque mauris nec. Fusce nunc, vel at, nam dignissim, nisl non donec volutpat. Dictum morbi laoreet eligendi eleifend. Vitae eget. 
+            Sed ac velit dui ante integer egestas, diam mi ultricies elit vehicula etiam luctus, felis adipiscing, wisi ac mi, id interdum. 
+            Malesuada vulputate et, libero eros adipiscing eu vestibulum mus tempus, euismod mollis tempus nec vel a, amet nec dictum vestibulum. 
+            Ipsum in mollis ultricies, ante condimentum eget eros nec pede, ligula nisl id cras diam, velit ut feugiat, condimentum montes lectus vulputate nibh.
+        </p>
+        <h3>By Sarah Gaug. November 26, 2017</h3>
+    </div>
+</article>
