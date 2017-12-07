4b9071638c50b21ce706974071c11c44b2308b74
bwilli16 <33357883+bwilli16@users.noreply.github.com>
2017-12-06 23:35:07 -0500

    Update essentials.php

--- essentials.php
+++ essentials.php
@@ -4,7 +4,7 @@ include ('top.php');
 
 <body>
  
-<div class="row">
+<div class="row" id='essentials'>
     <figure class="ingredient one">
         <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
         <figcaption> <b>Title of the FIRST Recipe </b><br>
@@ -30,34 +30,7 @@ include ('top.php');
             </figcaption>
     </figure>
 </div>
-    <div class="row">
-    <figure class="ingredient one">
-        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
-        <figcaption> <b>Title of the FIRST Recipe </b><br>
-                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
-            </figcaption>
-    </figure>
-    <figure class="ingredient two">
-        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
-        <figcaption> <b>Title of the SECOND Recipe</b> <br>
-                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
-            </figcaption>
-    </figure>
-    <figure class="ingredient three">
-        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
-        <figcaption> <b>Title of the FIRST Recipe </b><br>
-                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
-            </figcaption>
-    </figure>
-    <figure class="ingredient four">
-        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
-        <figcaption> <b>Title of the SECOND Recipe</b> <br>
-                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
-            </figcaption>
-    </figure>
-</div>
-</div>
-    <div class="row">
+    <div class="row" id='essentials'>
     <figure class="ingredient one">
         <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
         <figcaption> <b>Title of the FIRST Recipe </b><br>
@@ -84,7 +57,34 @@ include ('top.php');
     </figure>
 </div>
 </div>
-    <div class="row">
+    <div class="row" id='essentials'>
+    <figure class="ingredient one">
+        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
+        <figcaption> <b>Title of the FIRST Recipe </b><br>
+                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+    <figure class="ingredient two">
+        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
+        <figcaption> <b>Title of the SECOND Recipe</b> <br>
+                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+    <figure class="ingredient three">
+        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
+        <figcaption> <b>Title of the FIRST Recipe </b><br>
+                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+    <figure class="ingredient four">
+        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
+        <figcaption> <b>Title of the SECOND Recipe</b> <br>
+                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
+            </figcaption>
+    </figure>
+</div>
+</div>
+    <div class="row" id='essentials'>
     <figure class="ingredient one">
         <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
         <figcaption> <b>Title of the FIRST Recipe </b><br>
