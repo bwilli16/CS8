237264483a115ba973d0e45a0c4a8a2299bb43ef
sgaug <33431654+sgaug@users.noreply.github.com>
2017-12-07 01:13:09 -0500

    Update essentials.php

--- essentials.php
+++ essentials.php
@@ -6,109 +6,98 @@ include ('top.php');
  
 <div class="row" id='essentials'>
     <figure class="ingredient one">
-        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
-        <figcaption> <b>Title of the FIRST Recipe </b><br>
-                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
+        <img alt="Spices and herbs" class="Ingredient" src="../Images/spices.jpeg">
+        <figcaption> <b>Spice and herbs</b> <br>
+                These can be expensive to get from the grocery store, but they will help to make your food taste good.
+                Try getting them fresh from a farmer's market, or buy some basil and rosemary plants/seeds and grow your own!
             </figcaption>
     </figure>
     <figure class="ingredient two">
-        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
-        <figcaption> <b>Title of the SECOND Recipe</b> <br>
-                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
+        <img alt="Various starches" class="Ingredient" src="../Images/starches.jpg">
+        <figcaption> <b>Starches</b> <br>
+                These include bread, rice, pasta, potatoes, and other food rich in simple carbohydrates. While this food group gets
+                a bad rap, starches are incredibly versatile (you can use them to make practically anything and will keep for a long time.
             </figcaption>
     </figure>
     <figure class="ingredient three">
-        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
-        <figcaption> <b>Title of the FIRST Recipe </b><br>
-                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
+        <img alt="Yogurt" class="Ingredient" src="../Images/yogurt.png">
+        <figcaption> <b>Yogurt</b> <br>
+                Yogurt contains lots of probiotics and can be eaten by itself as a snack, with granola as a healthy breakfast, or as an ingredient
+                in several baking recipes. It comes in a lot of varieties and flavors, and there is even Greek yogurt for people who are lactose
+                or gluten intolerant!
             </figcaption>
     </figure>
     <figure class="ingredient four">
-        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
-        <figcaption> <b>Title of the SECOND Recipe</b> <br>
-                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
+        <img alt="Spilled salt shaker" class="Ingredient" src="../Images/salt.jpeg">
+        <figcaption> <b>Salt</b> <br>
+                It should go without saying, but salt is one of the most important ingredients you can have in your pantry. It adds flavor to your
+                favorite foods and is used in many baking recipes to slow down chemical reactions in the oven and balance out the flavors of the 
+                sweet ingredients.
             </figcaption>
     </figure>
 </div>
-    <div class="row" id='essentials'>
+<div class="row" id="essentials">
     <figure class="ingredient one">
-        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
-        <figcaption> <b>Title of the FIRST Recipe </b><br>
-                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
+        <img alt="Assorted blocks of cheeses" class="Ingredient" src="../Images/cheese.png">
+        <figcaption> <b>Cheese</b> <br>
+                For many people, this dairy staple is something they can't go without. Because cheese comes in so many forms and types, it can be used
+                to make practically anything. The key is to figure out what types of cheese you like on or in different types of foods. At the very least,
+                you should keep some shredded cheese, sliced cheese, cream cheese, and some cheese to put on/in your pasta.
             </figcaption>
     </figure>
     <figure class="ingredient two">
-        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
-        <figcaption> <b>Title of the SECOND Recipe</b> <br>
-                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
+        <img alt="Ground meat" class="Ingredient" src="../Images/groundmeat.jpg">
+        <figcaption> <b>Ground meat</b> <br>
+            For people who eat meat, this is usually the cheapest option to buy at the store. You can use it to make a variety of dishes (tacos, chili, pasta,
+            etc.) and can get it in many types (beef, poultry, turkey, etc.) to switch up the flavor and fat content. It also freezes well, so you don't have to
+            use it all at once!
             </figcaption>
     </figure>
     <figure class="ingredient three">
-        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
-        <figcaption> <b>Title of the FIRST Recipe </b><br>
-                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
+        <img alt="cooking oil containers" class="Ingredient" src="../Images/oils.jpg">
+        <figcaption> <b>Oils</b><br>
+                Cooking oils are a good source of fat for the body and help to make food taste good. The two basic kinds you'll need are olive oil (for low heat
+                meals like pasta) and canola oil (for high heat cooking, frying, and baking).
             </figcaption>
     </figure>
     <figure class="ingredient four">
-        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
-        <figcaption> <b>Title of the SECOND Recipe</b> <br>
-                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
+        <img alt="Frozen veggies" class="Ingredient" src="../Images/frozen.jpg">
+        <figcaption> <b>Frozen fruits and vegetables</b> <br>
+                Unlike most frozen foods, frozen fruits and vegetables often retain more vitamins and nutrients than fresh ones. These are great to stock up on when
+                there's a sale and keep them in your freezer. The trick is to know which ones reheat well and which ones don't. Generally, peas, berries, mushrooms,
+                and corn will stay crisp when you heat them up. You can also try freezing bananas when they're just about to go bad so you can save them to make a smoothie later.
             </figcaption>
     </figure>
 </div>
-</div>
-    <div class="row" id='essentials'>
+<div class="row" id="essentials">
     <figure class="ingredient one">
-        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
-        <figcaption> <b>Title of the FIRST Recipe </b><br>
-                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
+        <img alt="eggs" class="Ingredient" src="../Images/eggs.jpg">
+        <figcaption> <b>Eggs</b><br>
+                Eggs should be a staple in your fridge. They are a great source of protein, can be made in a variety of ways, and can be eaten by themselves or
+                added to a dish. They are also commonly used in baking recipes, so if you want to make a tasty treat for dessert, odds are you'll need eggs.
             </figcaption>
     </figure>
     <figure class="ingredient two">
-        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
-        <figcaption> <b>Title of the SECOND Recipe</b> <br>
-                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
+        <img alt="Flour in a bowl" class="Ingredient" src="../Images/flour.jpg">
+        <figcaption> <b>Flour</b> <br>
+                There a lot of different flours to choose from, but all-purpose flour will serve your general baking purposes and can also be used for
+                thickening sauces or coating foods before frying them. You can also get other types of flour if you are gluten sensitive or need a special
+                type of flour, such as when making bread.
             </figcaption>
     </figure>
     <figure class="ingredient three">
-        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
-        <figcaption> <b>Title of the FIRST Recipe </b><br>
-                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
+        <img alt="Assorted nuts" class="Ingredient" src="../Images/nuts.jpg">
+        <figcaption> <b>Nuts</b><br>
+                Nuts are good for your heart health and can make a tasty snack by themselves. You can also chop them up and add them as a topping to salads or meat,
+                make your own trail mix with them, or use them in baking recipes.
             </figcaption>
     </figure>
     <figure class="ingredient four">
-        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
-        <figcaption> <b>Title of the SECOND Recipe</b> <br>
-                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
+        <img alt="Chicken broth in a bowl" class="Ingredient" src="../Images/broth.jpg">
+        <figcaption> <b>Meat or vegetable broth</b> <br>
+                Some type of broth is a must have when it's a cold day or you're feeling under the weather and you want to make soup. Broth is also helpful when you
+                need to add moisture or flavor to a dish or to thin out a particularly heavy sauce.
             </figcaption>
     </figure>
-</div>
-</div>
-    <div class="row" id='essentials'>
-    <figure class="ingredient one">
-        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
-        <figcaption> <b>Title of the FIRST Recipe </b><br>
-                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
-            </figcaption>
-    </figure>
-    <figure class="ingredient two">
-        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
-        <figcaption> <b>Title of the SECOND Recipe</b> <br>
-                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
-            </figcaption>
-    </figure>
-    <figure class="ingredient three">
-        <img alt="Description of first image" class="Ingredient" src="../images/aspen-trees.jpg">
-        <figcaption> <b>Title of the FIRST Recipe </b><br>
-                The paragraph for the FIRST recipe will go here... <a href="project/recipes.php">See more recipes</a>
-            </figcaption>
-    </figure>
-    <figure class="ingredient four">
-        <img alt="Description of second image" class="Ingredient" src="../images/bristlecone-pine.jpg">
-        <figcaption> <b>Title of the SECOND Recipe</b> <br>
-                The paragraph for the SECOND recipe will go here... <a href="project/recipes.php">See more recipes</a>
-            </figcaption>
-    </figure>
-</div>
-    
+</div> 
 </body>
