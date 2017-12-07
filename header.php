daa1c7eab2cd75cd75c51a4c66046fcf31056573
bwilli16 <33357883+bwilli16@users.noreply.github.com>
2017-12-06 23:35:42 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
 <!-- ######################     Start of header   ############################## -->
-<header>
-    <h1>WORKING HEADER (our blog name)</h1>
+<header id='header'>
+    <h1> College Cooking </h1>
 </header>
