379e419f02655588c88f82386efbcf07a819fa37
sgaug <33431654+sgaug@users.noreply.github.com>
2017-12-07 00:23:28 -0500

    Update baking.php

--- baking.php
+++ baking.php
@@ -10,9 +10,46 @@ include ('top.php');
         <div id="space">
             <h5>.</h5>
         </div>
-        <h2>First recipe title</h2>
+        <h2>Muffin in a mug</h2>
         <figure>
-            <img alt="Pic of food" src="../images/aspen-trees.jpg">
+            <img alt="Chcocolate chip muffin in a mug" src="../Images/muffin.jpg">
+        </figure>
+        <p>
+            The perfect solution for when you want a muffin for dessert, but don't want to make a dozen, and only have a mug and a microwave.
+        </p>
+        <p><b>Ingredients:</b></p>
+        <ol>
+            <li>Individual size muffin mix pouch (mine is Duncan Hines chocolate chip muffin)</li>
+            <li>3 tablespoons of water</li>
+            <li>A mug with a handle</li>
+            <li>A spoon</li>
+        </ol>
+        <p>
+            The key to making this recipe is to follow the instructions on the box of muffin mix that you have. I will list what mine tells me to
+            do, but yours may be different.
+        </p>
+        <p>
+            Open the pouch and empty the powder into your mug. Measure out 3tbsp (literally 3 spoonfuls) of water and stir it into the mix until the 
+            batter is smooth.
+        </p>
+        <p>
+            Microwave on high for however long the box says (this will depend on the wattage of your microwave). Since my microwave has a lower wattage,
+            I had to microwave it for slightly longer at 1 min. 50 sec.\
+        </p>
+        <p>
+            Take the mug out of the microwave <strong>using the handle since it won't be as hot</strong> and let it cool for a few minutes before digging
+            in with a spoon. Perfect late night snack!
+        </p>
+        <h3>By Sarah Gaug. December 06, 2017</h3>
+    </div>
+    
+    <div>
+        <div id="space">
+            <h5>.</h5>
+        </div>
+        <h2>Second recipe title</h2>
+        <figure>
+            <img alt="Pic of food" src="../images/half-dome.jpg">
         </figure>
         <p><b>Ingredients:</b></p>
         <ol>
@@ -30,7 +67,7 @@ include ('top.php');
             Malesuada vulputate et, libero eros adipiscing eu vestibulum mus tempus, euismod mollis tempus nec vel a, amet nec dictum vestibulum. 
             Ipsum in mollis ultricies, ante condimentum eget eros nec pede, ligula nisl id cras diam, velit ut feugiat, condimentum montes lectus vulputate nibh.
         </p>
-        <h3>By Blake Williams. December 02, 2017</h3>
+        <h3>By Erin Brooks. November 29, 2017</h3>
     </div>
     
     <div>
