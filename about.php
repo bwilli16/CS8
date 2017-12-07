3d412531617d8642936363257841f7a7b6fd23fe
bwilli16 <33357883+bwilli16@users.noreply.github.com>
2017-12-06 23:34:30 -0500

    Update about.php

--- about.php
+++ about.php
@@ -4,21 +4,17 @@ include ('top.php');
 
 <body>
     <div id='about'>
-        <p>Lorem ipsum dolor sit amet, arcu nunc, aenean dolor sem, in mauris donec, pellentesque amet risus sed sem viverra volutpat, 
-            suspendisse eu adipiscing gravida. Mauris commodo semper odio aliquam, venenatis curabitur interdum nulla. Adipiscing cum enim, 
-            risus cum nunc dolor enim mattis odio, pede curabitur facilisis, egestas vel lectus lorem pellentesque cras sit, nec sodales accumsan
-            neque mauris nec. Fusce nunc, vel at, nam dignissim, nisl non donec volutpat. Dictum morbi laoreet eligendi eleifend. Vitae eget. 
-            Sed ac velit dui ante integer egestas, diam mi ultricies elit vehicula etiam luctus, felis adipiscing, wisi ac mi, id interdum. 
-            Malesuada vulputate et, libero eros adipiscing eu vestibulum mus tempus, euismod mollis tempus nec vel a, amet nec dictum vestibulum. 
-            Ipsum in mollis ultricies, ante condimentum eget eros nec pede, ligula nisl id cras diam, velit ut feugiat, condimentum montes lectus vulputate nibh.
+        <p> <b>If you would like to stay updated,</b> head over to our subscription page. Here you can 
+            subscribe to updates on cooking, baking, and nutrition tips & tricks, or any
+            combination of the above. Contact us via email if you wish to 
         </p>
     </div>
     <div id='col'>
-        <img id='portrait' alt="Portrait" src="../images/ice-in.jpg">
-        <p>Lorem ipsum dolor sit amet, arcu nunc, aenean dolor sem, in mauris donec, pellentesque amet risus sed sem viverra volutpat, 
-            suspendisse eu adipiscing gravida. Mauris commodo semper odio aliquam, venenatis curabitur interdum nulla. Adipiscing cum enim, 
-            risus cum nunc dolor enim mattis odio, pede curabitur facilisis, egestas vel lectus lorem pellentesque cras sit, nec sodales accumsan
-            neque mauris nec.</p>
+        <img id='portrait' alt="Portrait" src="../images/bioblake.jpg">
+        <p><b>Blake</b> is a junior at the University of Vermont, majoring in Data Science and
+            Mathematics. He enjoys eating large quantities of (mostly) healthy food, running, playing 
+            piano, and sunrise hikes with his homies. You're as likely to find him in the library as
+            you are on the track. His favorite food is red curry, assuming that chocolate milk doesn't count.</p>
     </div>
     <div id='col'>
         <img id='portrait' alt="Portrait" src="../images/ice-in.jpg">
@@ -28,7 +24,7 @@ include ('top.php');
             neque mauris nec.</p>
     </div>
     <div id='col'>
-        <img id='portrait' alt="Portrait" src="../images/biosarah">
+        <img id='portrait' alt="Portrait" src="../images/biosarah.jpg">
         <p><b>Sarah</b> is a freshman at the University of Vermont, majoring in Political Science and minoring in Public Policy Analysis. She enjoys
             reading (both classic and contemporary lit), traveling to new places, and snuggling with her Cocker Spaniel when she's back home in North Carolina.
             When she's not writing papers or cramming for a test, you can find her sipping coffee, knitting, and watching reruns of crime dramas.</p>
