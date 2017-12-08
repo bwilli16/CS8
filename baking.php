c6adedc3c9de6bd00f954570b342420ab2808b9c
erinbrooks <33432217+erinbrooks@users.noreply.github.com>
2017-12-07 22:02:10 -0500

    Update baking.php

--- baking.php
+++ baking.php
@@ -47,25 +47,39 @@ include ('top.php');
         <div id="space">
             <h5>.</h5>
         </div>
-        <h2>Second recipe title</h2>
+        <h2>Homemade (or dorm-made) Brownies:</h2>
         <figure>
             <img alt="Pic of food" src="../images/half-dome.jpg">
         </figure>
-        <p><b>Ingredients:</b></p>
+        <p><b>Ingredients</b></p>
         <ol>
-            <li>first </li>
-            <li>second </li>
-            <li>third </li>
-            <li>fourth ingredient </li>
+            <li>1 1/4 cups of sugar</li>
+            <li>1/4 cups of Water</li>
+            <li>2 ounces of unsweetened chocolate</li>
+            <li>3/4 cups of all purpose flour </li>
+            <li>1/3 cup of unsweetened cocoa powder </li>
+            <li>1/2 cups of semi sweet chocolate chips </li>
+            <li>1/4 teaspoon of baking powder </li>
+            <li>pinch of salt </li>
+            <li>2 large eggs </li>
+            <li>1/2 cup of vegetable oil </li>
+            <li>1 teaspoon of vanilla </li>
         </ol>
         <p>
-            Lorem ipsum dolor sit amet, arcu nunc, aenean dolor sem, in mauris donec, pellentesque amet risus sed sem viverra volutpat, 
-            suspendisse eu adipiscing gravida. Mauris commodo semper odio aliquam, venenatis curabitur interdum nulla. Adipiscing cum enim, 
-            risus cum nunc dolor enim mattis odio, pede curabitur facilisis, egestas vel lectus lorem pellentesque cras sit, nec sodales accumsan
-            neque mauris nec. Fusce nunc, vel at, nam dignissim, nisl non donec volutpat. Dictum morbi laoreet eligendi eleifend. Vitae eget. 
-            Sed ac velit dui ante integer egestas, diam mi ultricies elit vehicula etiam luctus, felis adipiscing, wisi ac mi, id interdum. 
-            Malesuada vulputate et, libero eros adipiscing eu vestibulum mus tempus, euismod mollis tempus nec vel a, amet nec dictum vestibulum. 
-            Ipsum in mollis ultricies, ante condimentum eget eros nec pede, ligula nisl id cras diam, velit ut feugiat, condimentum montes lectus vulputate nibh.
+           Preheat the oven to 350 degrees F. Spray a baking pan with cooking spray and set aside.
+        </p>
+        <p>
+           Combine sugar, water, and unsweetened chocolate in a small sauce pan. Cook over medium low heat
+           until the chocolate is melted and appears smooth, but the sugar will not be dissolved (about 3-5 minutes).
+           Let the mixture cool for about 5 minutes.
+        </p>
+        <p>
+           In a large bowl mix the flour, cocoa powder, chocolate chips, baking powder, and salt. Pour in the 
+           oil, eggs, and vanilla. Pour in the melted chocolate, and mix until everything is combined, but be careful not 
+           to over mix!
+        </p>
+        <p>
+           Pour batter into the greased pan and bake for 40-45 minutes. 
         </p>
         <h3>By Erin Brooks. November 29, 2017</h3>
     </div>
@@ -74,28 +88,35 @@ include ('top.php');
         <div id="space">
             <h5>.</h5>
         </div>
-        <h2>Second recipe title</h2>
+        <h2>Chocolate Chip Cookies</h2>
         <figure>
             <img alt="Pic of food" src="../images/half-dome.jpg">
         </figure>
         <p><b>Ingredients:</b></p>
         <ol>
-            <li>first </li>
-            <li>second </li>
-            <li>third </li>
-            <li>fourth ingredient </li>
+            <li>8 tablespoons of salted butter</li>
+            <li>1/2 cup of white sugar</li>
+            <li>1/4 cup packed with light brown sugar</li>
+            <li>1 teaspoon of vanilla </li>
+            <li>1 egg </li>
+            <li>1 1/2 cups of all purpose flour </li>
+            <li>1/2 teaspoon of baking soda </li>
+            <li>pinch of salt </li>
+            <li>3/4 cups of chocolate chips </li>
         </ol>
         <p>
-            Lorem ipsum dolor sit amet, arcu nunc, aenean dolor sem, in mauris donec, pellentesque amet risus sed sem viverra volutpat, 
-            suspendisse eu adipiscing gravida. Mauris commodo semper odio aliquam, venenatis curabitur interdum nulla. Adipiscing cum enim, 
-            risus cum nunc dolor enim mattis odio, pede curabitur facilisis, egestas vel lectus lorem pellentesque cras sit, nec sodales accumsan
-            neque mauris nec. Fusce nunc, vel at, nam dignissim, nisl non donec volutpat. Dictum morbi laoreet eligendi eleifend. Vitae eget. 
-            Sed ac velit dui ante integer egestas, diam mi ultricies elit vehicula etiam luctus, felis adipiscing, wisi ac mi, id interdum. 
-            Malesuada vulputate et, libero eros adipiscing eu vestibulum mus tempus, euismod mollis tempus nec vel a, amet nec dictum vestibulum. 
-            Ipsum in mollis ultricies, ante condimentum eget eros nec pede, ligula nisl id cras diam, velit ut feugiat, condimentum montes lectus vulputate nibh.
+            Preheat the oven to 350 degrees F. Microwave the butter for about 40 seconds, just so it is barely melted.
+        </p>
+        <p>
+           Beat the butter and sugar until creamy. Add the vanilla and the egg, and continue beating until combined. 
+        </p>
+        <p>
+           Add the flour, baking soda, and salt, and mix until crumbles form. Use your hands and press the crumbles together into a large ball 
+           of dough. Roll the dough into 12 even balls and place on the cookie sheet. Bake for 9-11 minutes, or until the cookies look puffy and dry, but just barely golden.
         </p>
         <h3>By Erin Brooks. November 29, 2017</h3>
     </div>
+    
     
     <div>
         <div id="space">
